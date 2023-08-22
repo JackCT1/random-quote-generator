545f3574001f172f2967006140e46ca069a6c512
JackCT1 <jackthomas933@gmail.com>
2023-08-22 22:49:08 +0100

    get request to api

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ class App extends React.Component {
   componentDidMount() {
     console.log("COMPONENT DID MOUNT");
   }
+
+  fetchAdvice = () => {
+    axios.get("https://api.adviceslip.com/advice");
+  };
+
   render() {
     return <h1>APP</h1>;
   }
