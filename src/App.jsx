d1dfecc4be9638551d05bd3cdc78d4cd7d49c1ed
JackCT1 <jackthomas933@gmail.com>
2023-08-22 21:42:18 +0100

    imported axios, started component did mount method

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import axios from "axios";
 
 class App extends React.Component {
   state = { advice: "" };
+
+  componentDidMount() {
+    console.log("COMPONENT DID MOUNT");
+  }
   render() {
     return <h1>APP</h1>;
   }
